9f78be523c466196786196237372f830908e265b
msingh24 <44597937+msingh24@users.noreply.github.com>
2018-10-30 02:00:25 -0400

    Todo Project

---
+++ Assets/js/Todos.js
@@ -0,0 +1,29 @@
+// This code will scratch out a specific Todo
+$("ul").on("click", "li", function(){
+//toggle = on and off for the line
+$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+ $(this).parent().fadeOut(500,function(){
+ 	$(this).remove();
+ });
+
+event.stopPropagation();
+
+});
+
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new Todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+	});
+$(".fa-plus").click(function(){
+ $("input[type='text']").fadeToggle()
+
+	});
